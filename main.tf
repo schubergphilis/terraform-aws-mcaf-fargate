8b31d4b86cf2ad3c6e44ff0b4ede73a3d6ceb40e
Tommy Maintz <tommy.maintz@gmail.com>
2020-10-27 10:30:36 +0100

    Make 0.13 compatible

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ locals {
 data "aws_region" "current" {}
 
 module "task_execution_role" {
-  source                = "github.com/schubergphilis/terraform-aws-mcaf-role?ref=v0.1.3"
+  source                = "github.com/schubergphilis/terraform-aws-mcaf-role?ref=v0.3.0"
   name                  = "TaskExecutionRole-${var.name}"
   principal_type        = "Service"
   principal_identifiers = ["ecs-tasks.amazonaws.com"]
