196bb6618ed95b88b85db91a383aa01b01e69650
lasseb <lbenninga@schubergphilis.com>
2021-03-11 17:34:05 +0100

    EIPs are only allowed for Network Load Balancers

--- variables.tf
+++ variables.tf
@@ -68,7 +68,7 @@ variable "image" {
 variable "load_balancer_eip" {
   type        = bool
   default     = false
-  description = "Whether to create Elastic IPs for the load balancer"
+  description = "Whether to create Elastic IPs for the load balancer (only supported when protocol == 'TCP')"
 }
 
 variable "load_balancer_internal" {
