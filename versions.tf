fe9723777a67690725179e3c2a3f27da05569abe
Frank van Boven <frank@cenotaph.nl>
2022-03-07 10:04:57 +0100

    Set correct version constraint

--- versions.tf
+++ versions.tf
@@ -2,7 +2,9 @@ terraform {
   required_version = ">= 0.13.0"
 
   required_providers {
-    source = "hashicorp/aws"
-    aws    = ">= 4.0.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0.0"
+    }
   }
 }
