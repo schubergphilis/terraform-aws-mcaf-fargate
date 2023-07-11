06010e0ecd478067fccdcfffd8f0b2caa930a0f7
Ergin Akin <ergousha@users.noreply.github.com>
2023-07-11 15:22:19 +0200

    fix array element

--- efs.tf
+++ efs.tf
@@ -5,12 +5,12 @@ resource "aws_efs_file_system" "default" {
 
 resource "aws_efs_mount_target" "mount" {
   count          = var.enable_efs ? 1 : 0
-  file_system_id = aws_efs_file_system.default.id
+  file_system_id = aws_efs_file_system.default[0].id
   subnet_id      = var.ecs_subnet_ids
   tags           = var.tags
 }
 
 resource "aws_efs_access_point" "default" {
   count          = var.enable_efs ? 1 : 0
-  file_system_id = aws_efs_file_system.default.id
+  file_system_id = aws_efs_file_system.default[0].id
 }
