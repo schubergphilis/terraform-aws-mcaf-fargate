ee58fc206dbe1739e97626da99f4046ad98bacba
Ergin Akin <ergousha@users.noreply.github.com>
2024-07-11 11:14:12 +0200

    set default to null

--- variables.tf
+++ variables.tf
@@ -86,7 +86,7 @@ variable "health_check" {
 variable "health_check_grace_period_seconds" {
   description = "Seconds to ignore failing load balancer health checks on newly instantiated tasks to prevent premature shutdown. Only valid for services configured to use load balancers."
   type        = number
-  default     = 0
+  default     = null
 }
 
 variable "image" {
